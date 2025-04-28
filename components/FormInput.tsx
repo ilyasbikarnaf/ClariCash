830f8616e0c155eec033d9d2d0519873a546ec5e
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-04-28 19:52:15 +0100

    create auth.ts helper functions

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -21,7 +21,7 @@ export default function FormInput({
   errors,
 }: FormInputProps) {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 ">
       <label htmlFor={type} className="opacity text-md ml-1">
         {labelText}
       </label>
@@ -30,7 +30,7 @@ export default function FormInput({
         type={type === "confirmPassword" ? "password" : type}
         name={type}
         id={type}
-        className=" h-10 w-full rounded-md border border-gray-300/10 bg-[#222222] px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-transparent disabled:cursor-not-allowed disabled:opacity-50 "
+        className="h-10 w-full rounded-[8px] border border-gray-300/10 bg-[#222222] px-3 py-2 text-md placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-transparent disabled:cursor-not-allowed disabled:opacity-50 "
       />
 
       {errors[type] && (
---
+++ lib/auth.ts
@@ -0,0 +1,95 @@
+import * as jose from "jose";
+import { compare, hash } from "bcrypt";
+import { cookies } from "next/headers";
+
+type JWTPayload = {
+  userId: string;
+  [key: string]: string | number | boolean | null | undefined;
+};
+
+const JWT_EXPIRATION = "7d";
+
+const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export async function hashPassword(password: string) {
+  return hash(password, 10);
+}
+
+export function verifyPassword(password: string, hashedPassword: string) {
+  return compare(password, hashedPassword);
+}
+
+export async function generateJWT(payload: JWTPayload) {
+  return new jose.SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime(JWT_EXPIRATION)
+    .sign(JWT_SECRET);
+}
+
+export async function verifyJWT(token: string): Promise<JWTPayload | null> {
+  try {
+    const { payload } = await jose.jwtVerify(token, JWT_SECRET);
+
+    return payload as JWTPayload;
+  } catch (error) {
+    console.log("JWT verification failed:", error);
+    return null;
+  }
+}
+
+export async function createSession(userId: string) {
+  try {
+    const token = await generateJWT({ userId });
+
+    const cookieStore = await cookies();
+    cookieStore.set({
+      name: "auth_token",
+      value: token,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 24 * 7,
+      path: "/",
+      sameSite: "lax",
+    });
+
+    return true;
+  } catch (error) {
+    console.log("Error creating cookie", error);
+    return false;
+  }
+}
+
+export async function getSession() {
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("auth_token")?.value;
+
+    if (!token) {
+      return null;
+    }
+
+    const payload = await verifyJWT(token);
+
+    return payload ? { userId: payload.userId } : null;
+  } catch (error) {
+    // Handle the specific prerendering error
+    if (
+      error instanceof Error &&
+      error.message.includes("During prerendering, `cookies()` rejects")
+    ) {
+      console.log(
+        "Cookies not available during prerendering, returning null session"
+      );
+      return null;
+    }
+
+    console.error("Error getting session:", error);
+    return null;
+  }
+}
+
+export async function deleteSession() {
+  const cookieStore = await cookies();
+  cookieStore.delete("auth_token");
+}
